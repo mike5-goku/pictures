aaf6dca6dbf65946624c7aaf8fe0462e79bb05bf
mike5-goku <dylanmacias173@gmail.com>
2025-05-12 17:18:42 -0700

    implementacion de un api para unsplash

---
+++ src/components/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const searchImages = async(term) =>{
+const url = 'https://api.unsplash.com/search/photos/?client_id=749275'
+const response = await axios.get(url ,{
+params: {
+    query : term
+}
+})
+
+console.log(response)
+return response.data.results
+}
+
+export default searchImages
