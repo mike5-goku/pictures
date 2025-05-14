aa88e7ddbf6d9404770c416a9009203be169061f
mike5-goku <dylanmacias173@gmail.com>
2025-05-14 13:43:29 -0700

    ahora

--- src/api.js
+++ src/api.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 const searchImages = async(term) =>{
-const url = 'https://api.unsplash.com/search/photos/?client_id=40a83d1b'
-const response = await axios.get(url ,{
-params: {
-    query : term
-}
-})
+
+    const response = await axios.get('http://www.omdbapi.com/', {
+               params: {
+                    apikey: '40a83d1b',
+                    s: term
+                    }
+              })
+            
+
+//const url = 'https://api.unsplash.com/search/photos/?client_id=40a83d1b'
+//const response = await axios.get(url ,{
+//params: {
+ //   query : term
+//}
+//})
 
 console.log(response)
 return response.data.results
