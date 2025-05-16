7819d8f48b0424f99b1b1018dae974968b6b1942
mike5-goku <dylanmacias173@gmail.com>
2025-05-15 20:11:29 -0700

    axios jala

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const searchImage = async(term) => {
-       
+//const url = 'https://api.unsplash.com/search/photos/?client_id=45kxOXjX5YFWzr5PArB9hUUP9gxNBdxvOEgJsAfnRqE'       
 const url = 'https://api.unsplash.com/search/photos/?client_id=45kxOXjX5YFWzr5PArB9hUUP9gxNBdxvOEgJsAfnRqE'
 const response = await axios.get(url ,{
 params: {
