0442ef99791d99db3e6a28e3df431ccb67e7ab3f
mike5-goku <dylanmacias173@gmail.com>
2025-05-15 19:35:53 -0700

    configuracion de axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
-import SearchBar from "./components/SearchBar"
+import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList"
-import searchImage from "./api.js"
+import searchImage from "./api";
 
 
 
